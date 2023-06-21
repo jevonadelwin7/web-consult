592ba03bfbd57dd3eee50c1f90dbbd474fb3d58e
jevonadelwin <jevonhura07@gmail.com>
2023-06-21 10:31:22 +0700

    update test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Layanan Administrasi, Pelaporan dan Konsultasi Inspektorat Kab. Kep. Mentawai</title>
+        <title>Layanan Administrasi, Pelaporan dan Konsultasi Inspektorat Kab. Kep. Mentawaii</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
